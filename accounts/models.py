d5fa76e19e3a8813a949d1cb8e13948fd7bbf0d0
alchemist1111 <emoyoeugine@gmail.com>
2025-08-11 23:39:56 +0300

    Created skeleton classes for UserManager, User and UserProfile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+# Class for the Base Custom User Manager
+class UserManager(BaseUserManager):
+    pass
+
+# Class for the User model inheriting from AbstractUser
+class User(AbstractUser):
+    pass
+
+
+# Class for the User Profile inheriting from models
+class UserProfile(models.Model):
+    pass
