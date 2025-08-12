5cb6758590dad6b16154b2061e006164a11b89d0
alchemist1111 <emoyoeugine@gmail.com>
2025-08-12 21:50:12 +0300

    Added the fields for the user model

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,27 @@ class UserManager(BaseUserManager):
 
 # Class for the User model inheriting from AbstractUser
 class User(AbstractUser):
-    pass
+    # User roles
+    USER_ROLES = (
+        ('buyer', 'Buyer'),
+        ('admin', 'Admin'),
+        ('vendor', 'Vendor'),
+    )
+    username = None # Overriding the default username field
+    email = models.EmailField(unique=True, db_index=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+    roles = models.CharField(max_length=50, choices=USER_ROLES, default='buyer')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True) 
+    
+    # Fields for use
+    USERNAME_FIELD = 'email' # Use email for authentication, not username
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    
+    objects = UserManager() # Use the custon user manager
+    
+    def __str__(self):
+        return f'Name:{self.first_name} {self.last_name}, Email:{self.email}'
 
 
 # Class for the User Profile inheriting from models
