dbc8c22a9233b488ddad6de286cafe4334397869
alchemist1111 <emoyoeugine@gmail.com>
2025-08-14 16:40:20 +0300

    Add serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import User, UserProfile
+from django.contrib.auth.password_validation import validate_password
 
 
+# User profile serializer
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+        
+        
 # User registration serializer
 class UserSerializer(serializers.ModelSerializer):
     """
@@ -10,16 +18,39 @@ class UserSerializer(serializers.ModelSerializer):
       - Enforces password confirmation and Django's password validators.
     
     """
-    # password = serializers.CharField(write_only=True, style={"input_type": "password"})
-    # password2 = serializers.CharField(write_only=True, style={"input_type": "password"})
-    # userprofile = UserProfileSerializer(required=False)
+    password = serializers.CharField(write_only=True, style={"input_type": "password"})
+    password2 = serializers.CharField(write_only=True, style={"input_type": "password"})
+    userprofile = UserProfileSerializer(required=False)
     
     class Meta:
         model = User
-        fields = '__all__'
-        # fields = ['email', 'first_name', 'last_name', 'phone_number', 'roles', "password", "password2", "userprofile"]
-        # extra_kwargs = {"roles": {"default": "buyer"}}
+        # fields = '__all__'
+        fields = ['email', 'first_name', 'last_name', 'phone_number', 'roles', "password", "password2", "userprofile"]
+        extra_kwargs = {"roles": {"default": "buyer"}}
+    
+    def validate(self, data):
+        """
+           Ensuring that the two password fields match and validate password strength.
+           
+        """
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('Passwords do not match.')
+        validate_password(data['password'])
+        return data
+    
+    def create(self, validated_data):
+        """
+          Create user and associated profile if provided.
+          
+        """
+        userprofile_data = validated_data.pop('userprofile', None)
+        user = User.objects.create_user(**validated_data)
         
+        if userprofile_data:
+            UserProfile.objects.create(user=user, **userprofile_data)
+
+        return user
+            
 # User registration serializer
 class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
