9644b629207529b4ad79781cc820774dc94214e1
alchemist1111 <emoyoeugine@gmail.com>
2025-08-14 11:20:08 +0300

    Add user management functions

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,29 @@ User = get_user_model() # Call the function to get user model defined
 
 # Class for the Base Custom User Manager
 class UserManager(BaseUserManager):
-    pass
+    """"
+         Create and return a user with an email and password.
+    """
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user 
+    
+    # Create a super user
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 # Class for the User model inheriting from AbstractUser
 class User(AbstractUser):
