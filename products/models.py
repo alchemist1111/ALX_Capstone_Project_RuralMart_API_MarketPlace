7a40490951ace474fb995e834992e3d3ee60ef88
alchemist1111 <emoyoeugine@gmail.com>
2025-08-16 12:00:21 +0300

    Add product and category models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+# Category model to represent product categories
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+
+# Product model to represent products in the rural mart
+class Product(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True, default='product_images/default.jpg')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.name} - Ksh.{self.price}'
+
