122acee6cba8e7100e2898527c0472c8dcbb8871
alchemist1111 <emoyoeugine@gmail.com>
2025-08-17 00:34:56 +0300

    Add products views for CRUD operations

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+# Product viewset
+class ProductViewset(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [AllowAny]
