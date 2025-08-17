52e82e85e6f219a8e523c9ba55dce3d18e04590e
alchemist1111 <emoyoeugine@gmail.com>
2025-08-17 04:48:58 +0300

    Add urls for cart & cart item

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+from .views import ProductViewSet, CategoryViewSet, CartViewSet, CartItemViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'carts', CartViewSet)
+router.register(r'cart-items', CartItemViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
