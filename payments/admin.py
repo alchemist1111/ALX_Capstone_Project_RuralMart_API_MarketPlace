13b7d2a3922a773e172de6d51dc294626b88ff5d
alchemist1111 <emoyoeugine@gmail.com>
2025-08-21 20:45:54 +0300

    Register payments models

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Payment, PaymentMethod, Transaction
 
-# Register your models here.
+admin.site.register(Payment)
+admin.site.register(PaymentMethod)
+admin.site.register(Transaction)
