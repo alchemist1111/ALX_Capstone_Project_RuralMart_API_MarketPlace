0cb6c107f9718b74e6acebebe268a3e268c46a27
alchemist1111 <emoyoeugine@gmail.com>
2025-08-22 11:31:20 +0300

    Add payment urls paths

---
+++ payments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import CreatePaymentView, ProcessPaymentView, PaymentStatusView
+
+urlpatterns = [
+    path('create-payment/', CreatePaymentView.as_view(), name='create-payment'),
+    path('process-payment/', ProcessPaymentView.as_view(), name='process-payment'),
+    path('payment-status/<int:payment_id>/', PaymentStatusView.as_view(), name='payment-status'),
+]
