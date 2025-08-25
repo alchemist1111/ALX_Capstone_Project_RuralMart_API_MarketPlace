73d563c7ada7214dd407891354beb180c242b20e
alchemist1111 <emoyoeugine@gmail.com>
2025-08-25 23:17:59 +0300

    Deployment

--- rural_mart/settings.py
+++ rural_mart/settings.py
@@ -143,12 +143,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Static files configuration
+STATIC_URL = '/static/'  # URL for serving static files
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Directory for collected static files (run python manage.py collectstatic)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # Use WhiteNoise for production static file handling
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# Media files configuration
+MEDIA_URL = '/media/'  # URL for serving media files
+MEDIA_ROOT = BASE_DIR / 'media'  # Directory for uploaded media files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
